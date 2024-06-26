c1dd534fe058608b4d8a224852fbeb10bb52937f
Oleksandr <aleksandrpopov3011@gmail.com>
2024-06-26 16:03:53 +0200

    adding a parent class Bird

--- project_Birds/project_Birds/Birds.h
+++ project_Birds/project_Birds/Birds.h
@@ -1,2 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Bird {
+protected:
+    string name;
+    string species;
+    double wingSpan;
+    string food;
+public:
+    Bird(string n, string s, double w, string f) : name(n), species(s), wingSpan(w), food(f) {}
+
+    virtual void displayInfo() const {
+        cout << "Name: " << name << endl;
+        cout << "Species: " << species << endl;
+        cout << "Wing Span: " << wingSpan << " cm" << endl;
+        cout << "Food: " << food << endl;
+
+
+    }
+    virtual void sound() const = 0;
+    virtual ~Bird()
+    {
+
+    }
+    virtual void behavior() const = 0;
+
+
+    string getName() const { return name; }
+    string getSpecies() const { return species; }
+    double getWingSpan() const { return wingSpan; }
+    string getFood() const { return food; }
+
+};
 
 
